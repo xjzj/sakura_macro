ba45a406feb339724792d86b6af5571d73e3279b
jin <xjinzhongjie@gmail.com>
2025-03-18 21:59:46 +0900

    inheritPrototype

--- inc.js
+++ inc.js
@@ -910,6 +910,16 @@ function make_range_square(range, inner_idx)
 	}
 }
 
+
+function inheritPrototype(subType, superType){
+	var o = new Object(superType.prototype);
+	o.constructor = subType;
+	subType.prototype = o;
+	// var prototype = Object.create(superType.prototype); // 创建对象，创建父类原型的一个副本
+	// prototype.constructor = subType;                    // 增强对象，弥补因重写原型而失去的默认的constructor 属性
+	// subType.prototype = prototype;                      // 指定对象，将新创建的对象赋值给子类的原型
+}
+
 function txt_type(){
 	/* StreamTypeEnum Values
 	*/
@@ -981,6 +991,9 @@ function  writeFile(code, path,list){
 
 }
 
+// 将父类原型指向子类
+inheritPrototype(readFile, txt_type);
+inheritPrototype(writeFile, txt_type);
 
 
 
